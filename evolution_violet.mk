dfb3821f27c767c3b759eba384ec29b5b198014d
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-09-01 22:52:28 +0530

    violet: Bring-up Evolution X * Add Evolution specific overlays * Add Evolution Official stuffs * Increase loop.max_part to 16 * Update copyrights
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

--- evolution_violet.mk
+++ evolution_violet.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2019-2020 The LineageOS Project
 # Copyright (C) 2018-2020 The SuperiorOS Project
+# Copyright (C) 2022 Evolution X
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,8 +9,8 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -17,23 +18,19 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Charging Animation
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
-# Face unlock
-TARGET_USES_FACE_UNLOCK := true
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_violet
+PRODUCT_NAME := evolution_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
     PRODUCT_NAME="violet"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# SuperiorOS
-SUPERIOR_OFFICIAL := true
+# Some Extra Stuffs.
+EVO_SUPPORT_URL := https://t.me/EvolutionXViolet
+BUILD_USERNAME := Yasin
+BUILD_HOSTNAME := Evolution-X
